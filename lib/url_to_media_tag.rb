3a9bc7ebdd6d3c7839b2e20e897707351eaba745
grosser <grosser.michael@gmail.com>
2011-08-04 11:01:01 +0200

    allow cascading settings in options

--- lib/url_to_media_tag.rb
+++ lib/url_to_media_tag.rb
@@ -1,13 +1,15 @@
 module UrlToMediaTag
   VERSION = File.read( File.join(File.dirname(__FILE__),'..','VERSION') ).strip
 
-  DEFAULTS = {
+  VIDEO_DEFAULTS = {
     :width => 640,
-    :height => 480
+    :height => 480,
+    :class => "url-to-media-tag-video",
+    :frameborder => 0
   }
 
   def self.video_iframe(url, options)
-    options = {:src => url, :class => "url-to-media-tag-video", :frameborder => 0}.merge(options)
+    options = {:src => url}.merge(VIDEO_DEFAULTS).merge(options)
     tag(:iframe, options)
   end
 
@@ -16,8 +18,18 @@ module UrlToMediaTag
     %{<#{name} #{options}></#{name}>}
   end
 
+  # :video, :x => 1, :settings => {:video => {:width => 300}}
+  # --> {:x => 1, :width => 300}
+  def self.merge_settings(*args)
+    options = args.pop.dup
+    raise unless options.kind_of?(Hash)
+    settings = options.delete(:settings) || {}
+    args.each{|type| options.merge!(settings[type] || {}) }
+    options
+  end
+
   def self.convert(url, options={})
-    options = DEFAULTS.merge(options)
+    options = options.dup
 
     # prevent any kind of html or xss
     return if url.include?('>') or url.include?('<')
@@ -26,11 +38,14 @@ module UrlToMediaTag
 
     # youtube
     when /http:\/\/(www.)?youtube\.com\/watch\?v=([A-Za-z0-9._%-]*)(\&\S+)?|http:\/\/(www.)?youtu\.be\/([A-Za-z0-9._%-]*)?/
+      options = merge_settings(:video, :vimeo, options)
       youtube_id = $2 || $5
       video_iframe "http://www.youtube.com/embed/#{youtube_id}", options
 
     # vimeo
     when /http:\/\/(www.)?vimeo\.com\/([A-Za-z0-9._%-]*)((\?|#)\S+)?/
+      options = merge_settings(:video, :vimeo, options)
+
       vimeo_id = $2
       show_title = "title=0" unless options.delete(:show_title)
       show_byline = "byline=0" unless options.delete(:show_byline)
@@ -42,6 +57,7 @@ module UrlToMediaTag
 
     # image
     when /https?:\/\/\S+\.(jpe?g|gif|png|bmp|tif)(\?\S+)?/i
+      options = merge_settings(:image, options)
       tag(:img, options.merge(:src => $&))
     end
 
--- spec/url_to_media_tag_spec.rb
+++ spec/url_to_media_tag_spec.rb
@@ -17,15 +17,15 @@ describe UrlToMediaTag do
     end
 
     it "converts images" do
-      UrlToMediaTag.convert('http://foo.com/foo.jpg').should == "<img height=\"480\" src=\"http://foo.com/foo.jpg\" width=\"640\"></img>"
-      UrlToMediaTag.convert('http://foo.com/foo.gif').should == "<img height=\"480\" src=\"http://foo.com/foo.gif\" width=\"640\"></img>"
-      UrlToMediaTag.convert('http://foo.com/foo.png').should == "<img height=\"480\" src=\"http://foo.com/foo.png\" width=\"640\"></img>"
-      UrlToMediaTag.convert('http://foo.com/foo.jpeg').should == "<img height=\"480\" src=\"http://foo.com/foo.jpeg\" width=\"640\"></img>"
-      UrlToMediaTag.convert('http://foo.com/foo.JPG').should == "<img height=\"480\" src=\"http://foo.com/foo.JPG\" width=\"640\"></img>"
+      UrlToMediaTag.convert('http://foo.com/foo.jpg').should == "<img src=\"http://foo.com/foo.jpg\"></img>"
+      UrlToMediaTag.convert('http://foo.com/foo.gif').should == "<img src=\"http://foo.com/foo.gif\"></img>"
+      UrlToMediaTag.convert('http://foo.com/foo.png').should == "<img src=\"http://foo.com/foo.png\"></img>"
+      UrlToMediaTag.convert('http://foo.com/foo.jpeg').should == "<img src=\"http://foo.com/foo.jpeg\"></img>"
+      UrlToMediaTag.convert('http://foo.com/foo.JPG').should == "<img src=\"http://foo.com/foo.JPG\"></img>"
     end
 
     it "converts images with ?" do
-      UrlToMediaTag.convert('http://foo.com/foo.jpg?foo=bar').should == "<img height=\"480\" src=\"http://foo.com/foo.jpg?foo=bar\" width=\"640\"></img>"
+      UrlToMediaTag.convert('http://foo.com/foo.jpg?foo=bar').should == "<img src=\"http://foo.com/foo.jpg?foo=bar\"></img>"
     end
 
     it "does not convert unknown" do
@@ -39,6 +39,33 @@ describe UrlToMediaTag do
     it "prevents xss" do
       UrlToMediaTag.convert('http://vimeo.com/26881896<').should == nil
       UrlToMediaTag.convert('http://vimeo.com/26881896>').should == nil
+    end
+
+    describe 'settings' do
+      it "uses directly given settings" do
+        UrlToMediaTag.convert('http://foo.com/foo.jpg', :foo => 'bar').should == "<img foo=\"bar\" src=\"http://foo.com/foo.jpg\"></img>"
+      end
+
+      it "uses indirectly given settings" do
+        UrlToMediaTag.convert('http://foo.com/foo.jpg', :settings => {:image => {:foo => 'bar'}}).should == "<img foo=\"bar\" src=\"http://foo.com/foo.jpg\"></img>"
+      end
+
+      it "does not use not-matching settings" do
+        UrlToMediaTag.convert('http://foo.com/foo.jpg', :settings => {:video => {:foo => 'bar'}}).should == "<img src=\"http://foo.com/foo.jpg\"></img>"
+      end
+
+      it "users more precise settings" do
+        UrlToMediaTag.convert('http://vimeo.com/26881896', :settings => {:video => {:foo => 'bar'}, :vimeo => {:foo => 'baz'}}).should include('foo="baz"')
+      end
+
+      it "overwrites default settings" do
+        UrlToMediaTag.convert('http://foo.com/foo.jpg', :foo => 'bar', :settings => {:image => {:foo => 'baz'}}).should include('foo="baz"')
+      end
+
+      it "can take options from settings" do
+        UrlToMediaTag.convert('http://vimeo.com/26881896').should include('title=0')
+        UrlToMediaTag.convert('http://vimeo.com/26881896', :settings => {:vimeo => {:show_title => true}}).should_not include('title=0')
+      end
     end
   end
 end
